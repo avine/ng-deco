565391fc9feecec86b57e32934ccde55f9cc395c
Stéphane Francel <stephane.francel@zenika.com>
2026-01-30 15:47:50 +0100

    Move lib folder...

--- projects/@avine/deco/src/index.ts.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of deco
- */
-
-export * from './lib/deco';
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,15 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterOutlet } from '@angular/router';
 import { LayoutModule } from './shared/layout';
 
+import { A } from '@avine/ng-deco/a';
+import { B } from '@avine/ng-deco/b';
+
 @Component({
   selector: 'app-root',
   imports: [MatButtonModule, MatIconModule, MatTooltipModule, RouterOutlet, LayoutModule],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  A = A;
+  B = B;
+}
