56dc72bf9e3f2107916036894a3411ea5eab7300
Stéphane Francel <stephane.francel@zenika.com>
2026-02-09 00:27:55 +0100

    Remove old implementations

--- projects/@avine/ng-deco/menu/index.ts
+++ projects/@avine/ng-deco/menu/index.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { DcMainMenu } from './main-menu/main-menu';
-import { DcSideMenuV2 } from './side-menu-v2/side-menu';
 import { DcSideMenuV3 } from './side-menu-v3/side-menu';
-import { DcSideMenu } from './side-menu/side-menu';
 
 export * from './main-menu/main-menu';
 export * from './menu-types';
-export * from './side-menu-v2/side-menu';
-export * from './side-menu-v2/side-menu-state';
-export * from './side-menu-v2/side-menu-template';
 export * from './side-menu-v3/side-menu';
-export * from './side-menu/side-menu';
 
-export const DcMenuModule = [DcMainMenu, DcSideMenu, DcSideMenuV2, DcSideMenuV3] as const;
+export const DcMenuModule = [DcMainMenu, DcSideMenuV3] as const;
 
 @NgModule({
   imports: [...DcMenuModule],
